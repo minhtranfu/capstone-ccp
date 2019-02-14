d66e9451dd588bb356d9e896da9d9305d83dd168
MinhTran <minhtranfu@gmail.com>
2019-02-15 00:04:07 +0700

    [web-client] Update equipment detail screen

--- client-web/src/services/domain/ccp-api-service.js
+++ client-web/src/services/domain/ccp-api-service.js
@@ -10,7 +10,10 @@ const CcpApiService = {
   },
   postEquipment(data) {
     return DataAccessService.post(`/equipments`, data);
-  }
+  },
+  getEquipmentById(id) {
+    return DataAccessService.get(`/equipments/${id}`);
+  },
 };
   
 export default CcpApiService;  
