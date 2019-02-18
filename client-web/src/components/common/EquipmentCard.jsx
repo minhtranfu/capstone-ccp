6143637c2690faf8dd5fd9642d1dc21fa3b1db68
MinhTran <minhtranfu@gmail.com>
2019-02-18 09:39:35 +0700

    [web-client] Update equipment card

--- client-web/src/components/common/EquipmentCard.jsx
+++ client-web/src/components/common/EquipmentCard.jsx
@@ -9,23 +9,24 @@ class EquipmentCard extends PureComponent {
         const { product: productData, className } = this.props;
         const product = productData.equipmentEntity;
         return (
-            <div className={`my-1 ${className}`}>
-                <div className="card">
-                    <Image src={product.image || 'https://via.placeholder.com/728x458.png?text=CCP+Capstone'} className="card-img-top" alt="" />
-                    <div className="card-body">
-                        <h6 className="card-title">{product.name} <span className="float-right"><i className="fa fa-dollar"></i> {product.dailyPrice}</span></h6>
-                        <div className="text-muted"><i className="fa fa-map-marker"></i> {product.address}</div>
-                        {product.availableTimeRanges[0] &&
-                            <div className="text-muted">
-                                <i className="fa fa-calendar-o"></i> {moment(product.availableTimeRanges[0].beginDate).format("YYYY/MM/DD")} - {moment(product.availableTimeRanges[0].endDate).format("YYYY/MM/DD")}
-                                {product.availableTimeRanges.length > 1 &&
-                                    <i> ({product.availableTimeRanges.length - 1} more)</i>
-                                }
-                            </div>
-                        }
-                        <Link to={`/equip-detail/${product.id}`} className="btn btn-outline-primary btn-sm">Chi tiết <i className="fa fa-chevron-right"></i></Link>
+            <div className={`equip-card my-1 ${className}`}>
+                <Link to={`/equip-detail/${product.id}`}>
+                    <div className="card">
+                        <Image src={product.image || 'https://via.placeholder.com/728x458.png?text=CCP+Capstone'} className="card-img-top" alt="" />
+                        <div className="card-body">
+                            <h6 className="card-title">{product.name} <span className="float-right"><i className="fa fa-dollar"></i> {product.dailyPrice}</span></h6>
+                            <div className="text-muted"><i className="fa fa-map-marker"></i> {product.address}</div>
+                            {product.availableTimeRanges[0] &&
+                                <div className="text-muted">
+                                    <i className="fa fa-calendar-o"></i> {moment(product.availableTimeRanges[0].beginDate).format("YYYY/MM/DD")} - {moment(product.availableTimeRanges[0].endDate).format("YYYY/MM/DD")}
+                                    {product.availableTimeRanges.length > 1 &&
+                                        <i> ({product.availableTimeRanges.length - 1} more)</i>
+                                    }
+                                </div>
+                            }
+                        </div>
                     </div>
-                </div>
+                </Link>
             </div>
         );
     }
