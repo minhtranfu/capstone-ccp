b3027c2e95ae867fba902b32fe4ac6db7ffcef8d
MinhTran <minhtranfu@gmail.com>
2019-02-22 13:25:58 +0700

    [web-client] Load data for dashboard my equipment

--- client-web/src/components/modules/user-dashboard/MyEquipments.jsx
+++ client-web/src/components/modules/user-dashboard/MyEquipments.jsx
@@ -1,10 +1,47 @@
 import React, { PureComponent } from 'react';
 
+import ccpApiService from '../../../services/domain/ccp-api-service';
+
 class MyEquipments extends PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            filterStatus: 'all',
+        };
+    }
+
+    _loadData = async () => {
+        const REQUESTER_ID = 12;
+        const equipments = await ccpApiService.getEquipmentsByContractorId(REQUESTER_ID);
+        this.setState({
+            equipments
+        });
+    };
+
+    componentDidMount() {
+        this._loadData();
+    }
+
     render() {
+        const { equipments } = this.state;
+
         return (
             <div>
                 <h4>My equipments</h4>
+                {equipments && equipments.map(equipment => {
+
+                    return (
+                            <div className="d-flex transaction my-3 rounded">
+                                <div className="image flex-fill">
+                                    <img src={equipment.thumbnailImage || "https://via.placeholder.com/300x200.png?text=CCP+Capstone"} />
+                                </div>
+                                <div className="detail flex-fill p-2">
+                                    <h6>{equipment.name}</h6>
+                                </div>
+                            </div>
+                    );
+                })}
             </div>
         );
     }
