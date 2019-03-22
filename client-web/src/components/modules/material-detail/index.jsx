0493e3518b0d21e35d77cc4ca6d6fd07bd96d95e
Minh Trần <minhtranfu@gmail.com>
2019-03-22 18:17:13 +0700

    [web-client] Refactor code material detail

--- client-web/src/components/modules/material-detail/index.jsx
+++ client-web/src/components/modules/material-detail/index.jsx
@@ -70,24 +70,33 @@ class MaterialDetail extends Component {
               <div className="row">
                 <div className="col-md-6 py-2">
                   <h6>
-                    <span className="text-muted"><i class="fal fa-calendar"></i> Posted:</span> {formatDate(material.createdTime)}
+                    <span className="text-muted"><i class="fal fa-calendar"></i> Posted: </span>
+                    {formatDate(material.createdTime) || <Skeleton width={100}/>}
                   </h6>
                 </div>
                 <div className="col-md-6 py-2">
                   <h6>
-                    <span className="text-muted"><i className="fal fa-tags"></i> Type:</span> {material.materialType ? material.materialType.name : <Skeleton width={100}/>}
+                    <span className="text-muted"><i className="fal fa-tags"></i> Type: </span>
+                    {material.materialType ? material.materialType.name : <Skeleton width={100}/>}
                   </h6>
                 </div>
                 <div className="col-md-6 py-2">
                   <h6>
-                    <span className="text-muted"><i className="fal fa-bullseye"></i> Construction:</span> {material.construction ? material.construction.name : <Skeleton width={100}/>}
+                    <span className="text-muted"><i className="fal fa-bullseye"></i> Construction: </span>
+                    {material.construction ? material.construction.name : <Skeleton width={100}/>}
                   </h6>
                 </div>
                 <div className="col-md-6 py-2">
-                  <h6><span className="text-muted"><i className="fal fa-money-bill"></i> Price:</span> {material.price ? <span>{formatPrice(material.price)}<i className="text-muted">/{material.unit}</i></span> : <Skeleton width={75}/>}</h6>
+                  <h6>
+                    <span className="text-muted"><i className="fal fa-money-bill"></i> Price: </span>
+                    {material.price ? <span>{formatPrice(material.price)}<i className="text-muted">/{material.unit}</i></span> : <Skeleton width={75}/>}
+                  </h6>
                 </div>
                 <div className="col-md-12 py-2">
-                  <h6><span className="text-muted"><i className="fal fa-map-marker"></i> Address:</span> {material.construction ? material.construction.address : <Skeleton width={250}/>}</h6>
+                  <h6>
+                    <span className="text-muted"><i className="fal fa-map-marker"></i> Address: </span>
+                    {material.construction ? material.construction.address : <Skeleton width={250}/>}
+                  </h6>
                 </div>
               </div>
               <h5 className="my-3 text-muted"><u>Description:</u></h5>
