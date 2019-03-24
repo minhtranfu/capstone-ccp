ab291e09053abafe43cf82859e36e2a8d081dcdc
Minh Trần <minhtranfu@gmail.com>
2019-03-24 19:11:30 +0700

    [web-client] Make footer logo smaller

--- client-web/src/components/common/Footer.jsx
+++ client-web/src/components/common/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
           <div className="col-md-12 mt-md-0 mt-3 text-center">
             <h5 className="text-uppercase mb-3">A capstone project at</h5>
             <img src="https://seeklogo.com/images/F/fpt-university-logo-B3B6D84292-seeklogo.com.png"
-              alt="FPT University logo"/>
+              alt="FPT University logo" height="80"/>
           </div>
         </div>
       </div>
