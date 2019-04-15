c0ce5d834cf0243ed4205081f8ee73db68d9c574
Minh Trần <minhtranfu@gmail.com>
2019-04-16 01:42:35 +0700

    [web-client] Fix bug firebase message in service worker

--- client-web/src/firebase-messaging-sw.js
+++ client-web/src/firebase-messaging-sw.js
@@ -1,9 +1,10 @@
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 
-if (firebase.messaging.isSupported()) {
+// console.log(firebase);
+// if (firebase.messaging.isSupported()) {
   firebase.initializeApp({
       messagingSenderId: "479160953028"
   });
   const messaging = firebase.messaging();
-}
+// }
