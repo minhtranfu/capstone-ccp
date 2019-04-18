8aebbe24a25c68c6c1ff777e2e181cb8edb92057
Minh Trần <minhtranfu@gmail.com>
2019-04-18 11:57:57 +0700

    [web-client] Set to default image if load error

--- client-web/src/components/common/Image.jsx
+++ client-web/src/components/common/Image.jsx
@@ -3,8 +3,27 @@ import LazyLoad from 'react-lazyload';
 import Skeleton from 'react-loading-skeleton';
 
 class Image extends PureComponent {
+  defaultSrc = 'https://via.placeholder.com/230x140';
+  state = {
+    isLoadError: false,
+  };
+
+  // Set to default image src on image
+  _handleLoadImageError = () => {
+    this.setState({
+      isLoadError: true,
+    });
+  };
+
   render () {
     const {circle, width, height, ...props} = this.props;
+    const { isLoadError } = this.state;
+
+    // Check if image load error
+    if (isLoadError) {
+      props.src = this.defaultSrc;
+    }
+
     return (
       <LazyLoad
         scroll
@@ -12,7 +31,7 @@ class Image extends PureComponent {
         height={height}
         placeholder={<Skeleton height={height} width={width} circle={circle}/>}
       >
-        <img {...props} height={height} width={width}/>
+        <img {...props} height={height} width={width} onError={this._handleLoadImageError}/>
       </LazyLoad>
     );
   }
